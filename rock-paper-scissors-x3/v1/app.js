b6fee8589142d8747c97d6e78b3fc9a04d867ed5
poncardasm <159589562+poncardasm@users.noreply.github.com>
2024-05-23 02:39:05 +0300

    feat: Rock button event

---
+++ rock-paper-scissors-x3/v1/app.js
@@ -0,0 +1,40 @@
+const playerChoiceDisplay = document.getElementById('player-choice');
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const resultDisplay = document.getElementById('result');
+
+const rockBtn = document.getElementById('rock');
+const paperBtn = document.getElementById('paper');
+const scissorsBtn = document.getElementById('scissors');
+
+// Choices
+const choices = ['🪨 Rock', '📃 Paper', '✂️ Scissors'];
+
+// Get computer choice
+function getComputerChoice() {
+  const choiceIndex = Math.floor(Math.random() * choices.length);
+  return choices[choiceIndex];
+}
+
+// Determine the result
+function getResult(playerChoice, computerChoice) {
+  if (playerChoice === computerChoice) {
+    return "🤝 It's a tie!";
+  }
+  if (
+    (playerChoice === '🪨 Rock' && computerChoice === '✂️ Scissors') ||
+    (playerChoice === '📃 Paper' && computerChoice === '🪨 Rock') ||
+    (playerChoice === '✂️ Scissors' && computerChoice === '📃 Paper')
+  ) {
+    return '🤩 You win!';
+  }
+  return '😭 You lose!';
+}
+
+// Button events
+rockBtn.addEventListener('click', () => {
+  const playerChoice = '🪨 Rock';
+  const computerChoice = getComputerChoice();
+  playerChoiceDisplay.textContent = playerChoice;
+  computerChoiceDisplay.textContent = computerChoice;
+  resultDisplay.textContent = getResult(playerChoice, computerChoice);
+});
